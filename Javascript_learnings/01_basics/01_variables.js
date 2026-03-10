9864e4da05f9cc88526e4e01c17dabc4d8dc609f
Yash Aditya Mishra <146587626+YashAditya1212@users.noreply.github.com>
2026-03-10 22:29:26 +0530

    Added git directory

---
+++ Javascript_learnings/01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId =12345
+
+let accountEmail = "yash@gmail.com"
+
+var accountPassword = "123456"
+
+accountCity="gorakhpur"
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ Javascript_learnings/01_basics/03.js
@@ -0,0 +1,7 @@
+let score ="100"
+console.log(typeof(score))
+
+let convert_to_number= Number(score)
+console.log(typeof(convert_to_number))
+
+console.log( "2" >1)
